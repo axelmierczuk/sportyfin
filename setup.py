d615c0b7fca829064cccf899b0874df7b07805a4
Axel <axelmierczuk@gmail.com>
2022-01-10 09:48:28 -0500

    Updated versioning.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sportyfin',
-    version='0.1.7',
+    version='0.1.8',
     author='Axel Mierczuk',
     author_email='axelmierczuk@gmail.com',
     packages=['sportyfin', 'sportyfin.util'],
